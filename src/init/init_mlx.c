b3e8f8b6f0882d656d3561d6399792af0b072ef0
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-09-09 20:17:54 -0300

    feat[init]: implement mlx initialization and window creation

--- src/init/init_mlx.c
+++ src/init/init_mlx.c
@@ -6,8 +6,25 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 20:37:09 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/09/08 20:46:08 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:17:11 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "game.h"
+
 //Init windown and confiure keys
+
+void	mlx_loop(t_classMlx *mlx)
+{
+	if (mlx)
+		mlx_loop(mlx->mlx);
+}
+
+t_classMlx	*init_mlx(void)
+{
+	t_classMlx	mlx;
+
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, 1920, 1080, "Hello world!");
+	return (&mlx);
+}
